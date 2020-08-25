4e4ced0a3a4223004be67b70e8c3e6c789f09ccf
Amjed Saleel K <amjedsaleel@gmail.com>
2020-08-25 17:23:33 +0530

    accessed data

--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'core/index.html')
+    result = requests.get('https://api.covid19api.com/summary')
+    print(result.json())
+    return render(request, 'core/index.html', {'result': result})
